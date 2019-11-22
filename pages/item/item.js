58234197b9e7b3cb358e2bfcc53f613929751b34
haoran <404620035@qq.com>
2019-11-22 15:13:28 +0800

    commit "list"

--- pages/item/item.js
+++ pages/item/item.js
@@ -1,3 +1,42 @@
+const app = getApp()
+
 Page({
-    
+    data: {
+        title: '',
+        movie: {}
+    },
+
+    // onReady 生命周期函数--监听页面初次渲染完成
+    onReady() {
+        wx.setNavigationBarTitle({
+            title: this.data.title + ' « 电影'
+        })
+    },
+
+    // 生命周期函数--监听页面加载
+    onLoad(params) {
+        wx.showLoading({ title: '拼命加载中...' })
+
+        app.douban.findOne(params.id).then(res => {
+            this.setData({
+                title: res.title,
+                movie: res
+            })
+            wx.setNavigationBarTitle({ title: res.title + ' « 电影' })
+            wx.hideLoading()
+        }).catch(e => {
+            this.setData({ title: '获取数据异常', movie: {} })
+            console.error(e)
+            wx.hideLoading()
+        })
+    },
+
+    // onShareAppMessage 小程序分享函数
+    onShareAppMessage(){
+        return {
+            title: this.data.title,
+            desc: this.data.title,
+            path: '/pages/item?id=' + this.data.id
+        }
+    }
 })
--- pages/list/list.js
+++ pages/list/list.js
@@ -1,3 +1,73 @@
+const app = getApp()
+
 Page({
-    
+    data: {
+        title: '',
+        subtitle: '加载中...',
+        type: 'in_theaters',
+        hasMore: true,
+        page: 1,
+        size: 20,
+        movies: []
+    },
+
+    // onReady 生命周期函数--监听页面初次渲染完成
+    onReady() {
+        wx.setNavigationBarTitle({
+            title: this.data.title + ' « 电影'
+        })
+    },
+
+    // onLoad 生命周期函数--监听页面加载
+    onLoad(params) {
+        this.data.title = params.title || this.data.title
+        this.data.type = params.type || this.data.type
+
+        this.loadMore()
+    },
+
+    loadMore() {
+        if (!this.data.hasMore) return
+        wx.showLoading({ title: '拼命加载中...' })
+        this.setData({ subtitle: '加载中...' })
+
+        return app.douban.find(this.data.type, this.data.page++, this.data.size).then(d => {
+            if (d.subjects.length) {
+                this.setData({
+                    subtitle: d.title,
+                    movies: this.data.movies.concat(d.subjects)
+                })
+            } else {
+                this.setData({ subtitle: d.title, hasMore: false })
+            }
+            wx.hideLoading()
+        }).catch(e => {
+            this.setData({ subtitle: '获取数据异常' })
+            console.error(e)
+            wx.hideLoading()
+        })
+    },
+
+    /**
+     * onPullDownRefresh 页面相关处理函数--监听用户下拉动作
+     * stopPullDownRefresh 刷新完成后停止刷新动作
+     */
+    onPullDownRefresh() {
+        this.setData({
+            movies: [],
+            page: 1,
+            hasMore: true
+        })
+        this.loadMore().then(() => {
+            wx.stopPullDownRefresh()
+        })
+    },
+
+    // onReachBottom 下拉加载函数
+    onReachBottom() {
+        this.loadMore()
+    }
+
+
+
 })
